fe4bf7ca9fc22ded28653973e1c20bce061b71a2
Mateusz Olszewski <matcrono@gmail.com>
2022-06-14 16:23:30 +0800

    Finished 6.4

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,26 +1,43 @@
 package com.kodilla.testing.shape;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ShapeCollector {
 
-    private ArrayList<Shape> shapeList = new ArrayList<>();
+    private List<Shape> shapeList = new ArrayList<>();
+
+    public List<Shape> getShapeList() {
+        return shapeList;
+    }
+
+    public int getShapeListSize() {
+        return shapeList.size();
+    }
 
     public void addFigure(Shape shape) {
-        // do nothing
+        shapeList.add(shape);
     }
 
     public boolean removeFigure(Shape shape) {
-        // return false temporarily
-        return false;
+        boolean result = false;
+        if (shapeList.contains(shape)) {
+            shapeList.remove(shape);
+            result = true;
+        }
+        return result;
     }
 
-    public int getFigure (int n) {
-        // return 100 temporarily
-        return 100;
+    public Shape getFigure(int n) {
+        if (n >= 0 && shapeList.size() >= n) {
+            return shapeList.get(n);
+        } else {
+            return null;
+        }
     }
-    public void showFigures () {
-        // do nothing
+
+    public String showFigures() {
+        return shapeList.toString();
     }
 }
 
--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -3,10 +3,11 @@ package com.kodilla.testing.shape;
 import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ShapeCollectorTestSuite {
-
-    ShapeCollector shapeCollector = new ShapeCollector();
 
     private static int testCounter = 0;
 
@@ -29,49 +30,66 @@ public class ShapeCollectorTestSuite {
 
     @Nested
     @DisplayName("Tests for ShapeCollector")
-    class testShapeCollector {
+    class TestShapeCollector {
+        @DisplayName("Testing Adding a figure")
         @Test
         void testAddFigure() {
             // Given
+            ShapeCollector shapeCollector = new ShapeCollector();
             Shape bigSquare = new Square(150);
             // When
             shapeCollector.addFigure(bigSquare);
             // Then
-            Assertions.assertEquals(shapeCollector.getFigure(150), bigSquare);
+            assertEquals(bigSquare, shapeCollector.getFigure(0));
         }
 
+        @DisplayName("Testing removing of the figure")
         @Test
         void testRemoveFigure() {
             // Given
+            ShapeCollector shapeCollector = new ShapeCollector();
             Shape smallCircle = new Circle(10);
             // When
             shapeCollector.addFigure(smallCircle);
             // Then
             boolean result = shapeCollector.removeFigure(smallCircle);
-            Assertions.assertTrue(result);
-
+            assertTrue(result);
+            assertEquals(0, shapeCollector.getShapeListSize());
         }
 
+        @DisplayName("Testing getting the figure")
         @Test
         void testGetFigure() {
 
             // Given
-            Shape mediumTriangle = new Triangle(50);
+            ShapeCollector shapeCollector = new ShapeCollector();
+            Shape smallTriangle = new Triangle(50);
+            Shape mediumTriangle = new Triangle(100);
+            Shape bigTriangle = new Triangle(150);
             // When
+            shapeCollector.addFigure(smallTriangle);
             shapeCollector.addFigure(mediumTriangle);
+            shapeCollector.addFigure(bigTriangle);
             // Then
-            shapeCollector.getFigure(1);
-            Assertions.assertEquals(mediumTriangle, mediumTriangle.getField()); // This is definitely wrong
+            assertEquals(smallTriangle, shapeCollector.getFigure(0));
+            assertEquals(mediumTriangle, shapeCollector.getFigure(1));
+            assertEquals(bigTriangle, shapeCollector.getFigure(2));
         }
 
+        @DisplayName("Testing showing all Figures in Array")
         @Test
         void testShowFigures() {
 
             // Given
-
+            ShapeCollector shapeCollector = new ShapeCollector();
+            Shape smallCircle = new Circle(20);
+            Shape bigCircle = new Circle(200);
             // When
-
+            shapeCollector.addFigure(smallCircle);
+            shapeCollector.addFigure(bigCircle);
+            List<Shape> shapeExpectedList = shapeCollector.getShapeList();
             // Then
+            assertEquals(shapeExpectedList.toString(), shapeCollector.showFigures());
         }
 
     }
