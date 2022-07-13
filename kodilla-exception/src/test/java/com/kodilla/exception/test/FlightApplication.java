2f38274b5642a643c9e04ba283cfc98deff84077
Mateusz Olszewski <matcrono@gmail.com>
2022-07-13 19:13:22 +0800

    finished 8.4

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/FlightApplication.java
@@ -0,0 +1,32 @@
+package com.kodilla.exception.test;
+
+public class FlightApplication {
+
+    public static void main (String[] args)  {
+        Flight flight1 = new Flight("Warsaw", "Tokyo");
+        Flight flight2 = new Flight("Shanghai", "Berlin");
+        Flight flight3 = new Flight("Seoul", "Czestochowa");
+
+        FlightFinder flightFinder = new FlightFinder();
+
+
+
+        try {flightFinder.findFlight(flight1);
+        System.out.println("Flying from airport " + flight1.getDepartureAirport()+ " to airport " + flight1.getArrivalAirport());
+        } catch (RouteNotFoundException e) {
+            System.out.println("Airport not found on the list : "  + e);
+        }
+
+        try {flightFinder.findFlight(flight2);
+            System.out.println("Flying from airport " + flight2.getDepartureAirport()+ " to airport " + flight2.getArrivalAirport());
+        } catch (RouteNotFoundException e) {
+            System.out.println("Airport not found on the list : "  + e);
+        }
+
+        try {flightFinder.findFlight(flight3);
+            System.out.println("Flying from airport " + flight3.getDepartureAirport()+ " to airport " + flight3.getArrivalAirport());
+        } catch (RouteNotFoundException e) {
+            System.out.println("Airport not found on the list : "  + e);
+        }
+    }
+}
---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,43 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+
+    private Map<String, Boolean> AirportMap;
+
+    public FlightFinder() {
+        AirportMap = airportDataBase();
+    }
+
+    private Map<String, Boolean> airportDataBase() {
+        Map<String, Boolean> airportDataBase = new HashMap<>();
+        airportDataBase.put("Shanghai", true);
+        airportDataBase.put("Tokyo", true);
+        airportDataBase.put("Warsaw", true);
+        airportDataBase.put("Helsinki", false);
+        airportDataBase.put("Rome", false);
+        airportDataBase.put("Berlin", true);
+        airportDataBase.put("Seoul", true);
+        airportDataBase.put("Amsterdam", false);
+        airportDataBase.put("Beijing", false);
+
+        return airportDataBase;
+    }
+
+    public boolean findFlight(Flight flight) throws RouteNotFoundException {
+        return findAirport(flight.getArrivalAirport()) && findAirport(flight.getDepartureAirport());
+    }
+
+
+    public boolean findAirport(String airportName) throws RouteNotFoundException {
+        if (airportDataBase().containsKey(airportName) && airportDataBase().containsValue(true)) {
+            return true;
+        } else if (airportDataBase().containsKey(airportName) && airportDataBase().containsValue(false)) {
+            throw new RouteNotFoundException("Airport is currently unavailable");
+        } else {
+            throw new RouteNotFoundException("Airport does not exist in the database");
+        }
+    }
+}
