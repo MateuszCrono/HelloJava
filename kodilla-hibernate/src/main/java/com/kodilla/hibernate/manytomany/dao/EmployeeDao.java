19853bbb9126a0b45d7e4246002bf121198940ef
Mateusz Olszewski <matcrono@gmail.com>
2022-11-03 15:42:37 +0800

    18.1 JSON practice

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
@@ -1,10 +1,10 @@
 package com.kodilla.hibernate.manytomany.dao;
 
 import com.kodilla.hibernate.manytomany.Employee;
-import com.kodilla.hibernate.task.Task;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import javax.transaction.Transactional;
 import java.util.List;
 
