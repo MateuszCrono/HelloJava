67dc58074d2c0fa7d6fd806e41983c69579634b8
Mateusz Olszewski <matcrono@gmail.com>
2023-02-03 11:12:10 +0800

    24.3

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/Order.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/Order.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class Order {
     private ProductService productService;
     private final List<Item> items = new ArrayList<>();
-    private final Long orderId;
-    private final Long userId;
+    private  Long orderId;
+    private  Long userId;
     private boolean isPaid;
     private boolean isVerified;
     private boolean isSubmitted;
